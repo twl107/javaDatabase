33876994f9c759f0f4b9ab52d1acc7ba4385622f
twl107 <ds1202kr@gmail.com>
2025-07-15 17:52:18 +0900

    20250715_1752 run수정,test삭제

--- src/t5_Hoewon/HoewonRun.java
+++ src/t5_Hoewon/HoewonRun.java
@@ -2,7 +2,7 @@ package t5_Hoewon;
 
 import java.util.Scanner;
 
-// 회원관리프로그램 최종본...(20250715_1727) 
+// 회원관리프로그램 최종본...(20250715_1727.........) 
 public class HoewonRun {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
